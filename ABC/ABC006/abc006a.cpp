160c75abe2c429eca33a6690e6e39e6e8476393d
sho-na <33408140+sho-na@users.noreply.github.com>
2018-03-05 23:47:50 +0900

    Create abc006a.cpp

---
+++ ABC/ABC006/abc006a.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int N;
+    scanf("%d",&N);
+    
+    if(N==3||N==6||N==9){
+        printf("YES\n");
+    }else{
+        printf("NO\n");
+    }
+    
+}
