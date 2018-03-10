6edf24656383bab585c95ac9298090f11332e102
sho-na <33408140+sho-na@users.noreply.github.com>
2018-03-11 01:36:52 +0900

    Create abc013a.cpp

---
+++ ABC/ABC013/abc013a.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    char x;
+    scanf("%s",&x);
+    
+    if(x=='A'){
+        printf("1\n");
+    }else if(x=='B'){
+        printf("2\n");
+    }else if(x=='C'){
+        printf("3\n");
+    }else if(x=='D'){
+        printf("4\n");
+    }else if(x=='E'){
+        printf("5\n");
+    }
+    
+}
